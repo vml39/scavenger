59e7bb421cd85d767a44e048260b9b8e7841bda0
Vivian Li <vml39@cornell.edu>
2019-11-09 17:43:09 -0500

    basic implementation

---
+++ src/components/Badge.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="badge">
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Badge',
+  props: {
+    name: String,
+    icon: String,
+    description: String,
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="profile">
+    <h2>Level {{ level }}</h2>
+    <img alt="profile image" src="" />
+    <h2>{{ name }}</h2>
+    
+    <div class="badges">
+        <Badge name="photographer" /> <!-- display on click -->
+    </div>
+    <div class="favvendors">
+        <FavVendor name="Apple Farms" />
+        <VendorInfo /> <!-- display on click -->
+    </div>
   </div>
 </template>
+
+<script>
+
+import Badge from '@/components/Badge.vue'
+import FavVendor from '@/components/FavVendor.vue'
+import VendorInfo from '@/components/VendorInfo.vue'
+
+export default {
+  name: "Profile",
+  components: {
+    Badge, 
+    FavVendor,
+    VendorInfo
+  },
+  data () {
+    return {
+      day: "",
+      forecast: "",
+      low: "", // convert int to string
+      high: "" // convert int to string
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
