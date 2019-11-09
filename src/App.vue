a98d92e5d74ce662ac8ecb0d840742b0fe9252f3
Vivian Li <vml39@cornell.edu>
2019-11-08 20:48:53 -0500

    views and components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Map</router-link> |
+      <router-link to="/profile">Profile</router-link> |
+      <router-link to="/vendors">Vendors</router-link>
     </div>
     <router-view/>
   </div>
