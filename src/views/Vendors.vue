ab23fee597d57fb63e8bdd5599d56549b856a815
Vivian Li <vml39@cornell.edu>
2019-11-14 15:36:18 -0500

    vendor directory setup

--- src/views/Vendors.vue
+++ src/views/Vendors.vue
@@ -1,4 +1,30 @@
 <template>
   <div class="vendors">
+    <h2>Vendor Directory</h2>
+    <!-- search bar -->
+    <!-- vendor info components -->
   </div>
 </template>
+
+<script>
+
+import VendorInfo from '@/components/VendorInfo.vue'
+import Nav from '@/components/Nav.vue'
+
+export default {
+  name: "Vendors",
+  components: {
+    VendorInfo, 
+    Nav
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
