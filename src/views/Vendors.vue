3fa773af2f59464fd139ef112f77dfaa8fab0c7e
Vivian Li <vml39@cornell.edu>
2019-11-14 15:48:55 -0500

    import search

--- src/views/Vendors.vue
+++ src/views/Vendors.vue
@@ -2,20 +2,23 @@
   <div class="vendors">
     <h2>Vendor Directory</h2>
     <!-- search bar -->
+    <autocomplete :search="search" placeholder="Search for a vendor" aria-label="Search for a vendor"></autocomplete>
     <!-- vendor info components -->
+
   </div>
 </template>
 
 <script>
 
 import VendorInfo from '@/components/VendorInfo.vue'
-import Nav from '@/components/Nav.vue'
+import Autocomplete from '@trevoreyre/autocomplete-vue'
+import '@trevoreyre/autocomplete-vue/dist/style.css'
 
 export default {
   name: "Vendors",
   components: {
     VendorInfo, 
-    Nav
+    Autocomplete
   },
   data () {
     return {
