12b41fe25d23c4a6864890fce61e88a88afda856
jnc83 <jnc83@cornell.edu>
2019-11-14 15:52:00 -0500

    nav branch updates

---
+++ src/components/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class = "nav">
+        <nav>
+            <router-link to="/map">Map</router-link>
+            <router-link to="/vendors">Vendor</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+   name: 'Nav'
+}
+</script>
+
+// Add import statements to map and vendor and merge to dev.  Do git checkout dev and get merge nav.
