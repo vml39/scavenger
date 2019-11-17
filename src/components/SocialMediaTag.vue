23bc45c3135cca8d460526c3ba150261d909fe86
Vivian Li <vml39@gmail.com>
2019-11-17 02:29:17 -0500

    list tags for a theme

---
+++ src/components/SocialMediaTag.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="socialmedia">
+    <h1>{{ theme }}</h1>
+    <ul>
+      <li :key="tag" v-for="tag in tags">
+        {{ tag }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SocialMediaTag',
+  props: {
+    theme: String
+  }, 
+  data: {
+    themes: {
+      "fall": ["fall", "thanksgiving", "pumpkinspice", "cider", "appleseason"],
+      "thanksgiving": ["turkey", "farmtotable", "pumpkin", "squash", "localfood"]
+    },
+    tags: []
+  },
+  methods: {
+    getTags (theme) {
+      this.tags = themes[theme];
+    }
+  },
+  mounted: function () {
+    this.getTags(this.theme);
+  },
+
+}
+</script>
