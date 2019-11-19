78a4411364b7b6b607ab90c5ee8dd58cd1ee4e69
Vivian Li <vml39@cornell.edu>
2019-11-19 00:20:50 -0500

    add text and button to social media popup

--- src/components/SocialMediaTag.vue
+++ src/components/SocialMediaTag.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="socialmedia">
     <h1>{{ theme }}</h1>
+    <p>Upload a photo to a social media platform of your choice using one these hashtags!</p>
     <ul>
       <li :key="tag" v-for="tag in tags">
-        {{ tag }}
+        #{{ tag }}
       </li>
     </ul>
+    <button>Done<button> <!-- onclick would close the modal -->
   </div>
 </template>
 
@@ -33,3 +35,9 @@ export default {
 
 }
 </script>
+
+<style scoped>
+  li {
+
+  }
+</style>
