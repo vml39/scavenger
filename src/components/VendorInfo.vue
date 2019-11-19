cf23abad99e0d5a9d9f41ef37e4d0db3ae7a5c8a
mac545 <mac545@cornell.edu>
2019-11-19 01:16:04 -0500

    edited vendorinfo

---
+++ src/components/VendorInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="vendorinfo">
+    <h1>{{ theme }}</h1>
+    <ul>
+      <li :key="vendorinfo" v-for="vendorinfo in vendorinfos">
+        {{ vendorinfo }}
+      </li>
+    </ul>
+    <v-dialog>
+        <v-card>
+            <v-card-title>
+                <h2>View Vendor Info</h2>
+            </v-card-title>
+        </v-card>
+    </v-dialog>
+  </div>
+</template>
