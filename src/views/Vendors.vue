1376f237a734b97a0c653b76a9a84959f0521914
Vivian Li <vml39@gmail.com>
2019-11-19 00:17:02 -0500

    modify vendor rendering

--- src/views/Vendors.vue
+++ src/views/Vendors.vue
@@ -3,7 +3,7 @@
     <h2>Vendor Directory</h2>
     <autocomplete :search="search" placeholder="Search for a vendor" aria-label="Search for a vendor"></autocomplete>
     <ul>
-      <li :key="vendor" v-for="vendor in tags">
+      <li :key="vendor" v-for="vendor in vendors">
         {{ vendor }} <!-- render each vendor component here -->
       </li>
     </ul>
