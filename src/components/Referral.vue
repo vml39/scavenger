2b216936ce6ce4446e77ca705e54e7c9da5cee8c
jnc83 <jnc83@cornell.edu>
2019-11-21 14:46:49 -0500

    referral modal

--- src/components/Referral.vue
+++ src/components/Referral.vue
@@ -14,7 +14,7 @@
 
             <div class="modal-header">
                 <slot name="header">
-                Refer a vendor to a friend!
+                <h3> Refer a vendor to a friend! </h3>
                 </slot>
             </div>
 
@@ -29,7 +29,6 @@
 
             <div class="modal-footer">
                 <slot name="footer">
-                default footer
                 <button class="modal-default-button" @click="$emit('close')">
                     Send
                 </button>
@@ -78,5 +77,81 @@ export default {
 </script>
 
 <style scoped>
+
+  .modal-mask {
+  position: fixed;
+  /* z-index: 9998; */
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  
+}
+
+.modal-body {
+  margin: 20px 0;
+  justify-content: left;
+  margin-bottom: 7px;
+}
+
+.modal-footer {
+  height: 20px;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+input {
+  margin-bottom: 10px;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+/* .modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+} */
+
+
  
 </style>
