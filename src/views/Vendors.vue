d6254916f4c162f3e5afcd1db27424c0f1c763f6
Vivian Li <vml39@gmail.com>
2019-11-21 15:32:35 -0500

    vendors json

--- src/views/Vendors.vue
+++ src/views/Vendors.vue
@@ -4,7 +4,7 @@
     <autocomplete :search="search" placeholder="Search for a vendor" aria-label="Search for a vendor"></autocomplete>
     <ul>
       <li :key="vendor" v-for="vendor in vendors">
-        {{ vendor }} <!-- render each vendor component here -->
+        <VendorInfo {{ vendor }} /> <!-- render each vendor component here -->
       </li>
     </ul>
   </div>
