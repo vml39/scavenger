72a1b555f4ec9fc5b0396e4e2f8a5cff86a1d0d2
Vivian Li <vml39@cornell.edu>
2019-11-21 21:58:40 -0500

    display vendors on page

--- src/components/VendorInfo.vue
+++ src/components/VendorInfo.vue
@@ -1,17 +1,56 @@
 <template>
   <div class="vendorinfo">
-    <h1>{{ theme }}</h1>
-    <ul>
-      <li :key="vendorinfo" v-for="vendorinfo in vendorinfos">
-        {{ vendorinfo }}
-      </li>
-    </ul>
-    <v-dialog>
-        <v-card>
-            <v-card-title>
-                <h2>View Vendor Info</h2>
-            </v-card-title>
-        </v-card>
-    </v-dialog>
+    <button @click="close">x</button>
+    <img class="logo" src="/img/vendorlogo.jpg" alt="vendor logo" />
+    <h1>{{ name }}</h1>
+    <p>Payment options: <img class="payment" v-if="cash" src="/img/cashicon.jpg" alt="cash" /><img class="payment" v-if="credit" src="/img/crediticon.jpg" alt="credit" /></p>
+    <p>Phone number: {{ phone }}</p>
+    <p>Email: {{ email }}</p>
+    <p v-if="hasSite">Website: {{ site }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'VendorInfo',
+  props: {
+    name: String,
+    phone: String,
+    email: String,
+    site: String,
+    cash: Boolean,
+    credit: Boolean
+  },
+  data: function () {
+    return {
+      hasSite: true
+    }
+  },
+  methods: {
+    close () {
+      // send vendor info false to the vendors page
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .vendorinfo {
+    margin-top: 10px;
+    background-color: lightgray;
+  }
+
+  .logo {
+    width: 40%;
+    margin: 10px auto 5px auto;
+    border-radius: 50%;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  .payment {
+    width: 10%;
+  }
+</style>
