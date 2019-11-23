ad9686d6723c2b68e52099c20c89b935354d7282
sb2355 <sb2355@cornell.edu>
2019-11-23 15:30:43 -0500

    added semantic ui

--- src/router.js
+++ src/router.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Map from './views/Map.vue'
 import Vendors from './views/Vendors.vue'
+import SuiVue from 'semantic-ui-vue'
+import 'semantic-ui-css/semantic.min.css'
 
+Vue.use(SuiVue)
 Vue.use(Router)
 
 export default new Router({
