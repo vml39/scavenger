754298eb13da8ac2f9183597f90645a7b42ae350
Vivian Li <vml39@gmail.com>
2019-11-26 15:23:50 -0500

    blur out background & vendors js

---
+++ src/assets/js/vendorsList.js
@@ -0,0 +1,103 @@
+let vendors = [
+  {
+    name: "Apple Farms",
+    phone: "1234567890",
+    email: "applefarms@ithaca.com",
+    site: "https://applefarms.com",
+    cash: true,
+    credit: false,
+    logo: "/img/applefarms.jpg",
+    // Source: https://www.thrillist.com/lifestyle/new-york/best-apple-picking-near-nyc
+    visited: true
+  },
+  {
+    name: "Abe Farms",
+    phone: "1234567890",
+    email: "abefarms@ithaca.com",
+    site: "https://abefarms.com",
+    cash: true,
+    credit: true,
+    logo: "/img/abefarms.jpg",
+    // Source: https://www.massaudubon.org/get-outdoors/wildlife-sanctuaries/drumlin-farm
+    visited: true
+  },
+  {
+    name: "Beecher's Cheese",
+    phone: "1234567890",
+    email: "beecherscheese@ithaca.com",
+    site: "https://beecherscheese.com",
+    cash: false,
+    credit: true,
+    logo: "/img/beecherscheese.jpg",
+    // Source: https://www.farmanddairy.com/top-stories/8-tips-to-start-a-dairy-farm/423679.html
+    visited: true
+  },
+  {
+    name: "Bethany's Botanical Garden",
+    phone: "1234567890",
+    email: "bethanysbotanicalgarden@ithaca.com",
+    site: "https://bethanysbotanicalgarden.com",
+    cash: true,
+    credit: false,
+    logo: "img/bethanysbotanicalgarden.jpg",
+    // Source: https://www.atlasobscura.com/articles/worlds-most-beautiful-botanical-gardens
+    visited: true
+  },
+  {
+    name: "Daisy Farm",
+    phone: "1234567890",
+    email: "daisyfarm@ithaca.com",
+    site: "https://daisyfarm.com",
+    cash: true,
+    credit: true,
+    logo: "/img/daisyfarm.jpg",
+    // Source: https://www.countryliving.com/gardening/a22109231/outdoor-daisy-flower-care/
+    visited: false
+  },
+  {
+    name: "Elena Eatery",
+    phone: "1234567890",
+    email: "elenaeatery@ithaca.com",
+    site: "https://elenaeatery.com",
+    cash: true,
+    credit: true,
+    logo: "img/elenaeatery.png",
+    // Source: https://emojipedia.org/fork-and-knife/
+    visited: false
+  },
+  {
+    name: "Georgian Winery",
+    phone: "1234567890",
+    email: "georgianwinery@ithaca.com",
+    site: "https://georgianwinery.com",
+    cash: true,
+    credit: true,
+    logo: "img/georgianwinery.jpg",
+    // Source: https://singletreewinery.com/abbotsfords-singletree-winery-opens-a-second-location-in-the-okanagan/
+    visited: false
+  },
+  {
+    name: "Indian Creek Farm",
+    phone: "1234567890",
+    email: "indiancreekfarm@ithaca.com",
+    site: "https://indiancreekfarm.com",
+    cash: false,
+    credit: true, 
+    logo: "img/indiancreekfarm.jpg",
+    // Source: https://www.visitithaca.com/attractions/indian-creek-farm
+    visited: false
+  },
+  {
+    name: "Renee's Bakery",
+    phone: "1234567890",
+    email: "reneesbakery@ithaca.com",
+    site: "https://reneesbakery.com",
+    cash: true,
+    credit: false,
+    logo: "img/reneesbakery.jpg",
+    // Source: https://dansfoods.com/departments/bakery
+    visited: false
+  }
+] 
+
+export default vendors
