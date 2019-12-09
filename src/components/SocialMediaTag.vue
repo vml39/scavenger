3ba9ad99c6cf2a64f2f18beeb3f7580584976abd
Vivian Li <vml39@gmail.com>
2019-12-09 14:23:08 -0500

    social media icons

--- src/components/SocialMediaTag.vue
+++ src/components/SocialMediaTag.vue
@@ -27,26 +27,50 @@ export default {
 <template>
   <div class="socialmediatag_modal">
     <h1 id="popupheader">Post on Social Media</h1>
-    <h1>{{ theme }}</h1>
     <p id="popupinstructions">
         Upload a photo to a social media platform of your choice using one these hashtags!
     </p>
-    <ul>
+    <ul class="tags">
       <li :key="tag" v-for="tag in tags">
         #{{ tag }}
       </li>
+    </ul>
+    <ul class="icons">
+      <li><a href="http://facebook.com/"><img src="/img/facebook.png" alt="Facebook"></a></li>
+      <!-- Source: https://seeklogo.net/facebook-icon-vector-91510.html -->
+      <li><a href="http://instagram.com/"><img src="/img/instagram.png" alt="Instagram"></a></li>
+      <!-- Source: https://en.instagram-brand.com/assets/icons -->
+      <li><a href="http://twitter.com/"><img src="/img/twitter.png" alt="Twitter"></a></li>
+      <!-- Source: https://www.iconfinder.com/icons/317723/social_social_media_tweet_twitter_icon -->
     </ul>
   </div>
 </template>
 
 
 <style scoped>
-  ul {
+  .tags {
     padding-left: 0;
   }
   
-  li {
+  .tags li {
     list-style: none;
     padding-left: 0;
+  }
+
+  .icons {
+    width: 50%;
+    margin: 0 auto 15px auto;
+    padding-left: 0;
+  }
+
+  .icons li {
+    width: 33%;
+    list-style: none;
+    display: inline-block;
+    padding: 0 5px;
+  }
+
+  img {
+    width: 100%;
   }
 </style>
