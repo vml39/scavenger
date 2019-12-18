3743e755030c37a17ef101ad00e1f3007b6e82cf
jnc83 <jnc83@cornell.edu>
2019-12-17 22:54:23 -0500

    vendor autofills

--- src/components/Referral.vue
+++ src/components/Referral.vue
@@ -4,19 +4,24 @@
         name: 'Referral',
         data  () {
           return {
-              email: '',
-              theVendor: '',
-              vendorName:'',
-              allVendors: vendorsList.default,
-              // visitedVendors: allVendors.default.filter(vendor => vendor.visited)
+            friendsEmail:'',
+            selectedVendor:'',
+            email: '',
+            theVendor: '',
+            vendorName:'',
+            allVendors: vendorsList.default,
+            // visitedVendors: allVendors.default.filter(vendor => vendor.visited)
           }      
         },
         methods: {
             store() {
-                this.email = document.getElementById("friendsEmail");
-                this.theVendor = document.getElementByID("selectedVendor");
+                this.friendsEmail = document.getElementById("friendsEmail");
+                this.selectedVendor = document.getElementByID("selectedVendor");
+               
             },
             completeTask() {
+              this.friendsEmail = '',
+              this.selectedVendor = '',
               this.email = "";
               this.theVendor = "",
               this.vendorName = ""
@@ -42,13 +47,13 @@
                 </div>
                 <div>
                     <label> Friend's Email: </label>
-                    <input v-model="friendsEmail" id= "friendsEmail">
+                    <input v-bind="friendsEmail" id= "friendsEmail">
                 </div>
                 
            
                 <div>
                   <button class="button" id="sendButton" @click="store">
-                    <a :href="'mailto:' + this.email + '?subject=Ithaca%20Farmers%20Market%20Vendor%20Referral&body=Hey!%20I%20just%20visited%20out%20'+ this.theVendor + '%20and%20they%20are%20awesome!%20Check%20them%20out%20next%20time%20you%20go%20to%20the%20farmers%20market!'">Send referral email</a>
+                    <a :href="'mailto:' + this.friendsEmail + '?subject=Ithaca%20Farmers%20Market%20Vendor%20Referral&body=Hey!%20I%20just%20visited%20'+ this.selectedVendor + '%20at%20the%20Ithaca%20Farmers%20Market%20and%20they%20are%20awesome!%20Check%20them%20out%20next%20time%20you%20go%20to%20the%20Farmers%20Market!'">Send referral email</a>
                   </button>
                   <sui-button color="green" @click="completeTask" content="Complete Task!" />
                 </div>
